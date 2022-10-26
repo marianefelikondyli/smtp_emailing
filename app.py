df4da20aa808318bd2623fd2358ead9cde7d1cf3
marianefelikondyli <nefikond@gmail.com>
2022-10-26 16:36:32 +0300

    updated some details

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, Response
+from flask import Flask
 import smtplib
+
 import os
 import sys
-from configparser import ConfigParser
+
 from email import encoders
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
@@ -13,41 +14,16 @@ from email.utils import formatdate
 app = Flask(__name__)
 
 
-HOST = "lala.server.com"
-SUBJECT = "my subject"
-TO = "nefeli.kondyli@uphellas.gr"
-FROM = "lala@mydomain.com"
-text = "Text of the email"
-FILE = "C:\\Users\\nefeli.kondyli\\Data Projects\\uninvoiced\\uninvoiced_2022-9-12_2022-9-26.xlsx"
-
-BODY = "\r\n".join((
-    "From: %s" % FROM,
-    "To: %s" % TO,
-    "Subject: %s" % SUBJECT,
-    "",
-    text
-    ))
-
-
 def send_email_with_attachment(subject, body_text, to_emails, file_to_attach):
     """
-    Send an email with an attachment
-    """
+         Send an email with an attachment
+         """
     base_path = os.path.dirname(os.path.abspath(__file__))
-    config_path = os.path.join(base_path, "email.ini")
-    header = 'Content-Disposition', 'attachment; filename="%s"' % file_to_attach
+    header = 'Content-Disposition', f'attachment; filename={file_to_attach}'
+    print(header)
 
-    # get the config
-    if os.path.exists(config_path):
-        cfg = ConfigParser()
-        cfg.read(config_path)
-    else:
-        print("Config not found! Exiting!")
-        sys.exit(1)
-
-    # extract server and from_addr from config
-    host = cfg.get("smtp", "server")
-    from_addr = cfg.get("smtp", "from_addr")
+    host = "prometheus.uphellas.gr"
+    from_addr = "bi@uphellas.gr"
 
     # create the message
     msg = MIMEMultipart()
@@ -55,9 +31,9 @@ def send_email_with_attachment(subject, body_text, to_emails, file_to_attach):
     msg["Subject"] = subject
     msg["Date"] = formatdate(localtime=True)
     if body_text:
-        msg.attach( MIMEText(body_text) )
+        msg.attach(MIMEText(body_text))
 
-    msg["To"] = ', '.join(to_emails)
+    msg["To"] = to_emails
 
     attachment = MIMEBase('application', "octet-stream")
     try:
@@ -68,27 +44,31 @@ def send_email_with_attachment(subject, body_text, to_emails, file_to_attach):
         attachment.add_header(*header)
         msg.attach(attachment)
     except IOError:
-        msg = "Error opening attachment file %s" % file_to_attach
+        msg = f"Error opening attachment file {file_to_attach}"
         print(msg)
         sys.exit(1)
 
     server = smtplib.SMTP(host)
     server.sendmail(from_addr, to_emails, msg.as_string())
     server.quit()
+    return "Email sent"
 
 
-@app.route('/', methods=['POST', 'GET'])
-def hello():
-    server = smtplib.SMTP(HOST)
-    server.sendmail(FROM, [TO], BODY)
+
+@app.route('/')
+def send_basic_email():
+    host = "prometheus.uphellas.gr"
+    server = smtplib.SMTP(host)
+    FROM = "bi@uphellas.gr"
+    TO = "nefeli.kondyli@uphellas.gr"
+    MSG = "Subject: LALALALALAL\n\nStavros sucks!!"
+    server.sendmail(FROM, TO, MSG)
     server.quit()
+    return "Email Send"
 
 
-@app.route('/test', methods=['POST', 'GET'])
-def test():
-    send_email_with_attachment(subject=SUBJECT, body_text=text, to_emails="nefeli.kondyli@uphellas.gr",
-                               file_to_attach=FILE)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/send_email')
+def send_email():
+    resp = send_email_with_attachment("My test email", "Here is your body", "nefeli.kondyli@uphellas.gr",
+                                      "./files/uninvoiced_2024-1-29_2024-2-29.xlsx")
+    return resp
